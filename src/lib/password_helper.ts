ed7b47ed27ab1a7a1eb04632e7a047e58095da32
MV Karan <mvkaran@github.com>
2024-01-12 10:41:13 +0530

    changes

--- src/lib/password_helper.ts
+++ src/lib/password_helper.ts
@@ -1,13 +1,20 @@
+/**
+ * Generates a random password of the specified length.
+ * @param length The length of the password. Default is 8.
+ * @returns The generated password.
+ */
 export function GeneratePassword(length: number = 8): string {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@$!%*?&';
     let password = '';
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
         password += characters[randomIndex];
     }
+    if (!IsValid(password)) {
+        return GeneratePassword(length);
+    }
     return password;
 }
-
 export function IsValid(password: string): boolean {
     const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     return regex.test(password);
